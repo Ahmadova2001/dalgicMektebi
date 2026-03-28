de9595854acf4d841379df939313ac8087bcaddb
Ahmadova2001 <ahmadovaa388@gmail.com>
2026-03-28 16:42:28 +0400

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'; // React-i import etmək vacibdir
+import React from 'react'; 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './assets/components/Header';
 import Main from './assets/components/Main';
